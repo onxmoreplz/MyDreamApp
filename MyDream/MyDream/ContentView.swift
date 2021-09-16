9336a9085fffc1ba509c0733ebe2ed6fd21ecdd3
onxmoreplz <hamdoyoung95@gmail.com>
2021-09-16 21:47:40 +0900

    [Chore] Initial IOS Project Build

---
+++ MyDream/MyDream/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyDream
+//
+//  Created by onemoreplz on 2021/09/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyDream/MyDream/MyDreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDreamApp.swift
+//  MyDream
+//
+//  Created by onemoreplz on 2021/09/16.
+//
+
+import SwiftUI
+
+@main
+struct MyDreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
